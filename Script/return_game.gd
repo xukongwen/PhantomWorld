4c89dfca585612ae6a56952f8236a99783e056ee
xukongwen <oabdao@gmail.com>
2020-02-28 10:46:26 +0800

    first

---
+++ Script/return_game.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+
+# 这就是传说中用按键转场了！
+func _on_Button_pressed():
+	get_tree().change_scene("res://scences/Control.tscn")
---
+++ scences/Control.gd
@@ -0,0 +1,38 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var scene_path_load
+
+# Called when the node enters the scene tree for the first time.
+# 取得每个按键对应要转场的信息
+func _ready():
+	# 这个是让键盘操作预先停留在新游戏按钮上
+	$Main/VBoxContainer/buttons/NewGameButton.grab_focus()
+	for button in $Main/VBoxContainer/buttons.get_children():
+		button.connect("pressed", self, "_on_pressed", [button.scene_to_load])
+
+# 按键下去之后先播放动画，要等动画播放完之后再切场景
+func _on_pressed(scene_to_load):
+	scene_path_load = scene_to_load
+	$ColorRect.show()
+	$ColorRect.fade_in()
+
+
+# 退出游戏
+func _on_Button_pressed():
+	get_tree().quit()
+
+# 用esc退出
+func _input(event):
+	if event is InputEventKey:
+		if (event as InputEventKey).scancode == KEY_ESCAPE:
+			get_tree().quit()
+		
+
+		
+# 动画播放完之后切场景
+func _on_ColorRect_fade_finished():
+	get_tree().change_scene(scene_path_load) 
