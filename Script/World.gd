c5883f4228077a0bbf1b5f8d25179ee507d5af16
徐空文 <oabdao@gmail.com>
2020-03-08 17:32:36 +0800

    初级的温度，有bug，时间系统需要大改

--- Script/World.gd
+++ Script/World.gd
@@ -1,14 +1,54 @@
-extends Object
+extends Node
 
-class World_dao:
+class_name World_dao, "res://Image/dao/world.png" #可以给一个icon！竟然！
 	
-	var dao = 0
-	var yin = -1
-	var yang = 1
+var dao = 0
+var yin = -1
+var yang = 1
 	
-	var yin_yang_now = 0
+var yin_yang_now = 0
 	
-	var temp = 0
-	var seazon = null
+var temp = 0
+var seazon = null
+
 	
+var second
+var minute
+var hour
+var day
+
+
+#################################################################################
+##################################Functions######################################
+#################################################################################
+	
+	
+#世界的时间功能
+func _init(time):
+	var int_time = int(floor(time))
+		
+	second = int_time % 60
+	minute = (int_time / 60) % 60
+	hour = (int_time / (60 * 60)) % 24
+	day = (int_time / (60 * 60 * 24))
+		
+#世界时间的事件激活功能
+func equals(second, minute, hour, day):
+	return self.second == second and self.minute == minute and self.hour == hour and self.day == day
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- text/time_test.gd
+++ text/time_test.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 
+var this_world : World_dao
+
 var time = 0
 var time_mult = 60
 var paused = false
 var time_scale = 1
 
-var prev_time = 0
 var deg = 0
+var tempture = 0
 
 signal time_passed(date_time)
 
@@ -17,64 +19,41 @@ func _ready():
 	connect("time_passed",self,"_on_time_passed")
 
 
-class DateTime:
-	
-	var second
-	var minute
-	var hour
-	var day
-#	var week
-#	var month
-#	var year
-
-	
-	
-	func _init(time):
-		var int_time = int(floor(time))
-		
-		second = int_time % 60
-		minute = (int_time / 60) % 60
-		hour = (int_time / (60 * 60)) % 24
-		day = (int_time / (60 * 60 * 24))
-		
-	func equals(second, minute, hour, day):
-		return self.second == second and self.minute == minute and self.hour == hour and self.day == day
-	
 
 func _process(delta):
 	
 	# 设定游戏事件和真实时间的差距
 	time += delta*time_mult
-	
-	var date_times = []
-	var int_time = floor(time) + 1
-	var prev_int_time = floor(prev_time) + 1
-	while prev_int_time < int_time:
-		date_times.append(prev_int_time)
-		prev_int_time += 1
-	
+
 	# 这个DateTime.new(time)就是获取秒，分，小时，日的时间信息结构
-	emit_signal("time_passed", DateTime.new(time))
+	emit_signal("time_passed", World_dao.new(time))
 	
-	prev_time = time
+	$time_lable.text = str("日:  ", World_dao.new(time).day,"  ", "时:  ", World_dao.new(time).hour,"点")
 	
-	$time_lable.text = str("日:  ", DateTime.new(time).day,"  ", "时:  ", DateTime.new(time).hour,"点")
-	
+	#deg = (0.1/60) * time_scale*time_mult
+		
+	# 这里是把太阳，月亮的位置（角度）和游戏时间匹配
+	$human_01/yinyang.rotation_degrees += (0.1/60)*time_scale/24*60
 	
 
 func _on_time_passed(date_times):
 	
 	if date_times.equals(0,0,12,0):
 		$event.text = "现在是正午"
+	
+	temp(date_times.hour)
 		
-	
-	deg = (0.1/60) * time_scale*time_mult
-	#$deg.text = str("每一秒增加：", deg*60," 度")
-	$deg.visible = false
 		
+func temp(time):
+	$deg.visible = true
 	
-	$human_01/yinyang.rotation_degrees += (0.1/60)*time_scale/24*60
+	if time <= 12 and time >0:
+		tempture += 1
+		
+	if time > 11 and time <= 23:
+		tempture -= 1
 	
+	$deg.text = str(tempture)
 
 func _on_Button_pressed():
 	get_tree().quit()
@@ -92,8 +71,8 @@ func _on_time5_pressed():
 	Engine.set_time_scale(time_scale)
 
 
+# 开关光
 func _on_light_pressed():
 	var light_stat = not $human_01/yinyang/yang/Light2D.enabled
 	$human_01/yinyang/yang/Light2D.enabled = light_stat
 	$ColorRect.visible = light_stat
-	pass # Replace with function body.
