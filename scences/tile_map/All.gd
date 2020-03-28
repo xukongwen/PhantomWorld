16207714f41b3f5db19ae9f5592e28509cd9aa1a
徐空文 <oabdao@gmail.com>
2020-03-28 20:38:36 +0800

    微调

--- scences/tile_map/All.gd
+++ scences/tile_map/All.gd
@@ -9,15 +9,22 @@ func _unhandled_input(event: InputEvent) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var tile_pos = world_to_map(mouse_pos)
 	var tile_cell_at_mouse_pos = get_cellv(tile_pos)
+
+	var t_name = tile_set.tile_get_name(get_cellv(tile_pos))
+	tt.get_node("Panel/Label").text = t_name
+	tt.rect_position = mouse_pos
+	print(mouse_pos)
+
+	if event is InputEventMouseMotion and tile_cell_at_mouse_pos == INVALID_CELL : 
+		tt.get_node("Panel/Label").text = "空的"
+		
+
+
 	
 	
 		
-	if event is InputEventMouseMotion and tile_cell_at_mouse_pos != INVALID_CELL : #hover tile
-		print(get_cellv(tile_pos))
-		var t_name = tile_set.tile_get_name(get_cellv(tile_pos))
-		tt.get_node("Panel/Label").text = t_name
+	
 		
-		tt.rect_position = mouse_pos
 	
 
 
